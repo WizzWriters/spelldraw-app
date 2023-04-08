9d1b5057ccf4617de376ae5917b503bff65e7f00
Ventus <ventuss550@gmail.com>
2023-04-08 20:14:43 +0200

    bugfix: fix pinner test

--- src/helpers/Pinner.ts
+++ src/helpers/Pinner.ts
@@ -1,6 +1,6 @@
 import _ from 'lodash'
 
 export default function pin(variable: string, value: any) {
-  if (import.meta.env.VITE_PINNER === 'TRUE')
+  if (import.meta.env.VITE_SHOW_PINS === 'TRUE')
     _.assign(window, { [variable]: value })
 }
