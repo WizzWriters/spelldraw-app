d68de2875260b508abf3b29bf5af04b67c8d45dd
Cezary Świtała <58264216+MusicFreak456@users.noreply.github.com>
2023-04-23 00:20:23 +0200

    Implement BezierShepeSmoother - service to describe polyline shapes with bezier curves  (#45)
    * Feature: Smooth drawing with bezier curves
    
    * Feature: Add drawing with bezier curves to HiddenCanvas

--- src/components/whiteboard/canvas/svg/SvgPolylineShape.vue
+++ src/components/whiteboard/canvas/svg/SvgPolylineShape.vue
@@ -1,20 +1,55 @@
 <script setup lang="ts">
 import { computed } from 'vue'
-import type { Polyline } from '@/services/canvas/Geometry'
+import type { Point, Polyline } from '@/services/canvas/Geometry'
+import BezierShapeSmoother, {
+  type BezierCurve
+} from '@/services/smoothing/BezierShapeSmoother'
 
 const props = defineProps<{
   shape: Polyline
 }>()
 
-let pointsListStr = computed(() => {
+function pointToString(point: Point) {
+  return `${point.xCoordinate} ${point.yCoordinate}`
+}
+
+function nextBezierSegment(bezierCurve: BezierCurve) {
+  switch (bezierCurve.controlPoints.length) {
+    case 0:
+      return ` L ${pointToString(bezierCurve.end)}`
+    case 1:
+      return (
+        ` Q ${pointToString(bezierCurve.controlPoints[0])},` +
+        ` ${pointToString(bezierCurve.end)}`
+      )
+    case 2:
+      return (
+        ` C ${pointToString(bezierCurve.controlPoints[0])},` +
+        ` ${pointToString(bezierCurve.controlPoints[1])},` +
+        ` ${pointToString(bezierCurve.end)}`
+      )
+    default:
+      return ''
+  }
+}
+
+let pathCommand = computed(() => {
   let pointList = props.shape.getPointList()
-  let pointListstr = pointList.reduce((prev, point) => {
-    return prev + ' ' + point.xCoordinate + ',' + point.yCoordinate
-  }, '')
-  return pointListstr
+  let bezierCurves = BezierShapeSmoother.getBezierCurves(pointList)
+  let result = ''
+
+  if (bezierCurves.length == 0) {
+    return ''
+  }
+
+  result += `M ${pointToString(bezierCurves[0].start)}`
+  for (let bezierCurve of bezierCurves) {
+    result += nextBezierSegment(bezierCurve)
+  }
+  return result
 })
 </script>
 
 <template>
-  <polyline :points="pointsListStr" fill="none" stroke="black" />
+  <path :d="pathCommand" fill="none" stroke="black" />
 </template>
