d9966b97ab3a6a5430ae49a2e87b52a7a72307bd
MusicFreak456 <czarek44564@gmail.com>
2023-04-24 01:06:34 +0200

    Feature: Add Pinia state management system

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import LoggerHelper from './helpers/LoggerHelper'
 
 import './assets/main.scss'
 LoggerHelper.initializeLogger()
 
-createApp(App).mount('#app')
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia)
+app.mount('#app')
