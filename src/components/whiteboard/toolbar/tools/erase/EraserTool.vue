54ef8eac55776d614e9368f532994701605ea011
MusicFreak456 <czarek44564@gmail.com>
2023-05-04 23:49:31 +0200

    Refactor: Simplify eraser logic

--- src/components/whiteboard/toolbar/tools/erase/EraserTool.vue
+++ src/components/whiteboard/toolbar/tools/erase/EraserTool.vue
@@ -48,8 +48,6 @@ const handlePointerEvent = (eventType: EPointerEvent, event: PointerEvent) => {
     case EPointerEvent.POINTER_UP:
     case EPointerEvent.POINTER_LEFT:
       isErasing.value = false
-      lastReading = null
-      toolbarStore.pointerHitline = null
       break
     case EPointerEvent.POINTER_MOVED: {
       toolbarStore.pointerHitline = getPointerHitline(pointerPosition)
@@ -79,12 +77,22 @@ onMounted(() => {
     hotspot: new Point(6, 3)
   }
 
+  function activateTool() {
+    toolbarStore.activeTool = { pointerIcon, handlePointerEvent }
+    logger.debug('Tool activated')
+  }
+
+  function deactivateTool() {
+    lastReading = null
+    toolbarStore.pointerHitline = null
+    logger.debug('Tool deactivated')
+  }
+
   watch(
     () => props.isActive,
     (newValue, oldValue) => {
-      if (oldValue || !newValue) return
-      toolbarStore.activeTool = { pointerIcon, handlePointerEvent }
-      logger.debug('Tool activated')
+      if (!oldValue && newValue) activateTool()
+      else if (oldValue && !newValue) deactivateTool()
     }
   )
 })
