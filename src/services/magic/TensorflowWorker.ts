6f18830453baaedecf925403c6978a0d61ca7fa8
Ventus <ventuss550@gmail.com>
2023-06-19 18:53:31 +0200

    refactor: remove top-level await

--- src/services/magic/TensorflowWorker.ts
+++ src/services/magic/TensorflowWorker.ts
@@ -4,22 +4,16 @@ import type { Dictionary } from 'lodash'
 import LoggerHelper from '@/helpers/LoggerHelper'
 import type { Tensor } from './TensorflowModel'
 
-async function call(x: Tensor) {
+async function call(model: tf.LayersModel, x: Tensor) {
   const input = tf.expandDims(x)
   const output = (model.call(input, {}) as [tf.Tensor<tf.Rank>])[0]
   logger.trace(input, '===>', output)
   return await output.array()
 }
 
-function meta() {
+function meta(model: tf.LayersModel) {
   logger.trace(model)
   return model.getConfig().name as Dictionary<any>
-}
-
-async function handleMessage(message: MessageEvent) {
-  const port = message.ports[0]
-  if (!message.data) port.postMessage(meta())
-  else port.postMessage(await call(message.data))
 }
 
 LoggerHelper.initializeLogger()
@@ -31,7 +25,15 @@ self.onmessage = (message: MessageEvent) => queue.push(message)
 const currenUrl = new URL(import.meta.url)
 const modelUrl =
   currenUrl.origin + import.meta.env.BASE_URL + `models/${self.name}/model.json`
-const model = await tf.loadLayersModel(modelUrl)
-logger.debug('Model initialized!', model)
-queue.map(handleMessage)
-self.onmessage = handleMessage
+
+tf.loadLayersModel(modelUrl).then((model) => {
+  async function handleMessage(message: MessageEvent) {
+    const port = message.ports[0]
+    if (!message.data) port.postMessage(meta(model))
+    else port.postMessage(await call(model, message.data))
+  }
+
+  logger.debug('Model initialized!', model)
+  queue.map(handleMessage)
+  self.onmessage = handleMessage
+})
