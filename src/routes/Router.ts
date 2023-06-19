dd02dcb6b5edeb23f14bbd5e9654dc9d03deef5f
MusicFreak456 <czarek44564@gmail.com>
2023-06-19 22:38:57 +0200

    Add loading page screen

--- src/routes/Router.ts
+++ src/routes/Router.ts
@@ -1,26 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import TheWhiteboard from '@/components/whiteboard/TheWhiteboard.vue'
 import NotFound from '@/views/NotFound.vue'
-import { useBoardStore } from '@/store/BoardStore'
+import PageLoading from '@/views/PageLoading.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'root',
-    redirect: () => {
-      return { name: 'board-create' }
-    }
-  },
-  {
-    path: '/board/create',
-    name: 'board-create',
-    beforeEnter: async () => {
-      const boardStore = useBoardStore()
-      await boardStore.createBoard()
-      return { name: 'board', params: { id: boardStore.boardId } }
-    },
-    component: NotFound
-  },
+  { path: '/', name: 'root', component: PageLoading },
   { path: '/board/:id', name: 'board', component: TheWhiteboard },
   { path: '/:pathMatch(.*)*', name: 'not-found', component: NotFound }
 ]
