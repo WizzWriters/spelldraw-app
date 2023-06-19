fee685132f32c6b529368b92528e773247535686
Cezary Świtała <58264216+MusicFreak456@users.noreply.github.com>
2023-06-19 23:11:37 +0200

    Loading page screen ⏲️  (#110)
    * Add loading page screen
    
    * Move main board component to views

--- src/routes/Router.ts
+++ src/routes/Router.ts
@@ -1,27 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import TheWhiteboard from '@/components/whiteboard/TheWhiteboard.vue'
+import TheBoard from '@/views/TheBoard.vue'
 import NotFound from '@/views/NotFound.vue'
-import { useBoardStore } from '@/store/BoardStore'
+import PageLoading from '@/views/PageLoading.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'root',
-    redirect: () => {
-      return { name: 'board-create' }
-    }
-  },
-  {
-    path: '/board/create',
-    name: 'board-create',
-    beforeEnter: async () => {
-      const boardStore = useBoardStore()
-      await boardStore.createBoard()
-      return { name: 'board', params: { id: boardStore.boardId } }
-    },
-    component: NotFound
-  },
-  { path: '/board/:id', name: 'board', component: TheWhiteboard },
+  { path: '/', name: 'root', component: PageLoading },
+  { path: '/board/:id', name: 'board', component: TheBoard },
   { path: '/:pathMatch(.*)*', name: 'not-found', component: NotFound }
 ]
 
---
+++ src/views/PageLoading.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { useBoardStore } from '@/store/BoardStore'
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+onMounted(async () => {
+  const boardStore = useBoardStore()
+  await boardStore.createBoard()
+  router.push({ name: 'board', params: { id: boardStore.boardId } })
+})
+</script>
+
+<template>
+  <div class="pageloader is-active">
+    <span class="title">Loading...</span>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.pageloader {
+  bottom: unset;
+  top: unset;
+  left: unset;
+  right: unset;
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+</style>
