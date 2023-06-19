28dc59f240fe4775df0fe2468eddfce74aa1ca0e
MusicFreak456 <czarek44564@gmail.com>
2023-06-19 19:57:20 +0200

    Fix magic loader

--- src/components/whiteboard/magic/ShapeCorrection.vue
+++ src/components/whiteboard/magic/ShapeCorrection.vue
@@ -36,7 +36,6 @@ function startCorrection() {
 
 async function commitCorrection() {
   loaderState.value.isTrackingPointer = false
-  loaderState.value.isLoading = false
   let correction = await correctionPromise
 
   if (!correction) {
@@ -46,6 +45,7 @@ async function commitCorrection() {
   }
 
   loaderState.value.wasCorrectionSuccessful = true
+  loaderState.value.isLoading = false
   canvasStore.currentlyDrawnShape = null
   canvasStore.addDrawnShape(correction)
   logger.debug('Shape correction commited')
@@ -75,13 +75,13 @@ function handleTransitionFromIdle(nextState: ECorrectionRequestState) {
   }
 }
 
-function handleTransitionFromStarted(nextState: ECorrectionRequestState) {
+async function handleTransitionFromStarted(nextState: ECorrectionRequestState) {
   switch (nextState) {
     case ECorrectionRequestState.IDLE:
       hideLoader()
       break
     case ECorrectionRequestState.COMMIT:
-      commitCorrection()
+      await commitCorrection()
       setTimeout(hideLoader, 300)
       break
     default:
