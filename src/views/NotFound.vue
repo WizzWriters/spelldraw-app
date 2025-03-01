4614c199cddddd2c96b058a4b468262e6c9ede49
MusicFreak456 <czarek44564@gmail.com>
2025-03-01 13:59:44 +0100

    Make not found page a bit more passable

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="container">
-    <h2 class="is-size-1 mt-2">404 - Not found :(</h2>
+  <div class="container is-flex is-align-items-center">
+    <div class="container">
+      <h2 class="is-size-1 mt-5">We couldn't find this board :(</h2>
+      <h2 class="is-size-4 mt-5">
+        Please refresh this page to create a new board.
+      </h2>
+    </div>
   </div>
 </template>
